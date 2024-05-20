2991fdbeef8a16012ca3c4ce7faf229ffe957906
Ashes <ashkan.eddyking@gmail.com>
2024-05-20 12:38:12 +0330

    modified:   src/components/LandingContent.js
    	modified:   src/components/LandingContent.js

--- src/components/LandingContent.js
+++ src/components/LandingContent.js
@@ -1,54 +1,54 @@
-import React from 'react'
-import image from './images/profile-picture1.jpg'
-import { useGlobalContext } from '../context'
-import { MdOutlineWeb } from 'react-icons/md'
-import SlowText from './UI/SlowText'
-import { Link } from 'react-router-dom'
+import React from "react";
+import image from "./images/profile-picture1.jpg";
+import { useGlobalContext } from "../context";
+import { MdOutlineWeb } from "react-icons/md";
+import SlowText from "./UI/SlowText";
+import { Link } from "react-router-dom";
 
 const LandingContent = () => {
-  const { setIsBurgerMenu } = useGlobalContext()
+  const { setIsBurgerMenu } = useGlobalContext();
   return (
     <>
-      <div onClick={() => setIsBurgerMenu(false)} className='about-container'>
-        <div className='picture-container'>
-          <img className='profile-picture' src={image} alt='Profile' />
+      <div onClick={() => setIsBurgerMenu(false)} className="about-container">
+        <div className="picture-container">
+          <img className="profile-picture" src={image} alt="Profile" />
         </div>
-        <div className='about-text'>
-          <SlowText text={'Front-End Developer'} speed={120} />
-          <h1 className='name'>Ashkan Eslami</h1>
+        <div className="about-text">
+          <SlowText text={"Front-End Developer"} speed={120} />
+          <h1 className="name">Ashkan Eslamii</h1>
           <p>Developing Responsive web pages using React library.</p>
-          <button className='download-btn'>
-            <a href='https://drive.google.com/uc?export=download&id=1R602c8puPfGGRDcuJ1wEyrWFFQqdJqgI'>
+          <button className="download-btn">
+            <a href="https://drive.google.com/uc?export=download&id=1R602c8puPfGGRDcuJ1wEyrWFFQqdJqgI">
               Download CV
             </a>
           </button>
-          <button className='contact-btn'>
-            <Link to={'/contact'}>Contact</Link>
+          <button className="contact-btn">
+            <Link to={"/contact"}>Contact</Link>
           </button>
         </div>
       </div>
-      <div className='abilities'>
-        <div className='abilities-header'>
+      <div className="abilities">
+        <div className="abilities-header">
           <span>What I Do</span>
         </div>
-        <div className='ability-item'>
-          <MdOutlineWeb className='ability-item-icon' />
+        <div className="ability-item">
+          <MdOutlineWeb className="ability-item-icon" />
           <h3>Web Development</h3>
           <p>
             Designing responsive web pages for any devices using React library
             and JavaScript language.
           </p>
         </div>
-        <div className='ability-item'>
-          <MdOutlineWeb className='ability-item-icon' />
+        <div className="ability-item">
+          <MdOutlineWeb className="ability-item-icon" />
           <h3>Data Scraping</h3>
           <p>
             Data Scraping and mining using python language and Scrapy library.
           </p>
         </div>
-        <div className='ability-item'>
-          <MdOutlineWeb className='ability-item-icon' />
-          <div className='ability-item-texts'>
+        <div className="ability-item">
+          <MdOutlineWeb className="ability-item-icon" />
+          <div className="ability-item-texts">
             <h3>Data Entry and Research</h3>
             <p>
               Professional research and handling the result in documents like
@@ -56,8 +56,8 @@ const LandingContent = () => {
             </p>
           </div>
         </div>
-        <div className='ability-item '>
-          <MdOutlineWeb className='ability-item-icon' />
+        <div className="ability-item ">
+          <MdOutlineWeb className="ability-item-icon" />
           <h3>English Transcriber</h3>
           <p>
             Transcription of English audio and videos. Also making subtitles for
@@ -66,6 +66,6 @@ const LandingContent = () => {
         </div>
       </div>
     </>
-  )
-}
-export default LandingContent
+  );
+};
+export default LandingContent;
